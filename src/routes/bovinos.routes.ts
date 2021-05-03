2189cb69d6cfa9bbb478e9a29654834c491b584a
Marcos Renan Krul <marcoskrul2k@gmail.com>
2021-05-03 13:40:22 -0300

    protecao das rotas com middleware para autenticacao

--- src/routes/bovinos.routes.ts
+++ src/routes/bovinos.routes.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import BovinoController from "../controllers/BovinoController";
+import authMiddleware from "../middlewares/ensureUserAuth";
 
 const routes = Router();
 const bovinoController = new BovinoController();
 
 
 routes.get('/api/bovino', bovinoController.index);
-routes.post('/api/bovino', bovinoController.create);
-routes.get('/api/selects', bovinoController.selects);
-routes.put('/api/bovino/:id', bovinoController.update);
-routes.delete('/api/bovino/:id', bovinoController.delete);
+routes.post('/api/bovino', authMiddleware, bovinoController.create);
+routes.get('/api/selects', authMiddleware, bovinoController.selects);
+routes.put('/api/bovino/:id', authMiddleware, bovinoController.update);
+routes.delete('/api/bovino/:id', authMiddleware, bovinoController.delete);
 
 
 export default routes;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import UserController from "../controllers/UserController";
+import authMiddleware from "../middlewares/ensureUserAuth";
 
 const routes = Router();
 const userController = new UserController();
 
 
-routes.get('/api/users', userController.index);
+routes.get('/api/users', authMiddleware, userController.index);
 routes.post('/api/users', userController.create);
 
 
